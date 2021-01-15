222e9ecfb509d20b3dd6a4f2e053938b6ab8a1cc
Kausar Mehmood <khan.kausar@gmail.com>
2021-01-15 11:18:07 +0500

    test cases project added

---
+++ AMSQC_Web.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace AMSQC_Web.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int i = 0;
+            Assert.True(i > 0);
+        }
+    }
+}
